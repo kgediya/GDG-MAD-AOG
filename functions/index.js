43a1cb509836fb4c9284a717a456752a27f3b2c8
Krunal Bharat Gediya <kgediya0898@gmail.com>
2019-01-09 18:46:35 +0530

    modified test-api.json

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,9 @@
  
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion,Image} = require('dialogflow-fulfillment');
+const {Payload,Card, Suggestion,Image} = require('dialogflow-fulfillment');
 const fetch = require('node-fetch');
-const url = "https://us-central1-gdgmadevents.cloudfunctions.net/event/";
+const url = "https://raw.githubusercontent.com/kgediya/GDG-MAD-AOG/master/test-api.json";
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
@@ -32,7 +32,7 @@ async function meetupQuery(agent){
              imageUrl: `${data.imageUrl}`,
              text: `${data.data}`,
              buttonText: 'RSVP',
-             buttonUrl: `https://www.meetup.com/gdg-mad/`
+             buttonUrl: `${data.meetupUrl}`
         }));
     agent.add(new Suggestion({
       title:'Venue',
@@ -47,12 +47,74 @@ async function meetupQuery(agent){
       platform:'ACTIONS_ON_GOOGLE'
     }));
     //Set context for followup events
-    agent.setContext({ name: 'meetup', lifespan: 2, parameters: { agenda: data.agenda,venue_title:data.venue.address.split('\n')[0],venue:data.venue.address, map:data.venue.link}});
+    agent.setContext({ name: 'meetup', lifespan: 4, parameters: { agenda: data.agenda,speaker1:data.agenda[0].speaker,speaker2:data.agenda[1].speaker,speaker3:data.agenda[2].speaker,venue_title:data.venue.address.split('\n')[0],venue:data.venue.address, map:data.venue.link}});
 }
+async function agendaQuery(agent){
+  const a11yText = 'Google Assistant Bubbles';
+const googleUrl = 'https://google.com';
 
+  agent.add("This is test response");
+  const data = await fetchInfo();
+  agent.add(new Payload(agent.ACTIONS_ON_GOOGLE,{
+    "expectUserResponse": true,
+    "richResponse": {
+      "items": [
+        {
+          "simpleResponse": {
+            "textToSpeech": "Inspiring People on the way!",
+            "displayText":"Here are the speakers for the day"
+          }
+        },
+        {
+          "carouselBrowse": {
+            "items": [ 
+              {
+                "title": `${data.agenda[0].speaker.name}`,
+                "openUrlAction": {
+                  "url": `https://twitter.com/${data.agenda[0].speaker.twitter}`
+                },
+                "description": `${data.agenda[0].name}`,
+                "footer": `${data.agenda[0].start}-${data.agenda[0].end}`,
+                "image": {
+                  "url": `https://avatars.io/twitter/${data.agenda[0].speaker.twitter}`,
+                  "accessibilityText": "Google Assistant Bubbles"
+                }
+              },
+              {
+                "title": `${data.agenda[1].speaker.name}`,
+                "openUrlAction": {
+                  "url": `https://twitter.com/${data.agenda[1].speaker.twitter}`
+                },
+                "description": `${data.agenda[1].name}`,
+                "footer": `${data.agenda[1].start}-${data.agenda[1].end}`,
+                "image": {
+                  "url": `https://avatars.io/twitter/${data.agenda[1].speaker.twitter}`,
+                  "accessibilityText": "Google Assistant Bubbles"
+                }
+              },
+              {
+                "title": `${data.agenda[2].speaker.name}`,
+                "openUrlAction": {
+                  "url": `https://twitter.com/${data.agenda[2].speaker.twitter}`
+                },
+                "description": `${data.agenda[2].name}`,
+                "footer": `${data.agenda[2].start}-${data.agenda[2].end}`,
+                "image": {
+                  "url": `https://avatars.io/twitter/${data.agenda[2].speaker.twitter}`,
+                  "accessibilityText": "Google Assistant Bubbles"
+                }
+              }
+            ]
+          }
+        }
+      ]
+    },
+    "userStorage": "{\"data\":{}}"
+  }));
+}
 async function fetchInfo(){
 
-  const page = await fetch("https://us-central1-gdgmadevents.cloudfunctions.net/event/"); //Fetch the API response
+  const page = await fetch("https://raw.githubusercontent.com/kgediya/GDG-MAD-AOG/master/test-api.json"); //Fetch the API response
   const response =await page.json(); //Parse the response into valid JSON object
   console.log(response);
   var descri = response[0].desc; //Extract the description data from the response
@@ -64,8 +126,9 @@ async function fetchInfo(){
     date: response[0].date,
     data: descri,
     venue : response[0].venue,
-    imageUrl: 'https://media.giphy.com/media/DCOgUFTPoCWqGLoyc7/giphy.gif',
-    agenda : response[0].agenda
+    imageUrl: response[0].img_link,
+    agenda : response[0].agenda,
+    meetupUrl: response[0].meetup_link
   }
 }
 
@@ -74,6 +137,7 @@ async function fetchInfo(){
   let intentMap = new Map();
   intentMap.set('Meetup Query',meetupQuery);
   intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Speaker Query',agendaQuery);
   intentMap.set('Default Fallback Intent', fallback);
   agent.handleRequest(intentMap);
 });
